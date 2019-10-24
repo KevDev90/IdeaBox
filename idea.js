681bcda8e1990efe5c4fb0dda084177e4472bed3
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2019-10-24 16:40:56 -0600

    Will add functionality to delete button

--- idea.js
+++ idea.js
@@ -1,8 +1,8 @@
 class Idea {
-  constructor(title, body) {
+  constructor(title, body, id) {
     this.title = title;
     this.body = body;
-    this.id = Date.now();
+    this.id = id || Date.now();
     this.star = false;
   }
 
